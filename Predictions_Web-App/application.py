2ce34a48301d4edd3a92280a09488b96e5166a62
Rushabh Bhatt <rushaxbh910@gmail.com>
2025-02-17 16:02:05 -0500

    project update

---
+++ Predictions_Web-App/application.py
@@ -0,0 +1,41 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+application=Flask(__name__)
+app=application
+
+#import ridge regressor and standard scalar pickle
+ridge_model=pickle.load(open(r"C:\Projects\Algerian_Forest_Fires_Prediction_End-to-End_ML-PROJECT\Flask\models\ridge_model.pkl",'rb'))
+standard_scaler=pickle.load(open(r"C:\Projects\Algerian_Forest_Fires_Prediction_End-to-End_ML-PROJECT\Flask\models\scaler.pkl",'rb'))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/predictdata",methods=['GET','POST']) #GET OPENS A STATIC PAGE, WHEREAS IF YOU SEARCH SOMETHING IN THAT STATIC PAGE WE'RE POSTING/SENDING A QUREY
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_scaled_data=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_scaled_data)
+
+        return render_template('home.html',result=result[0])
+    else:
+        return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
